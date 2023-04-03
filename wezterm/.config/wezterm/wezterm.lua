46441b2dfc1bc31477234f4e6fe8acb75458f6c8
YANGandyoungzbc <1371252549@qq.com>
2023-04-03 20:46:41 +0800

    Update wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -162,35 +162,63 @@ return {
 
     -- Aesthetic Night Colorscheme
     bold_brightens_ansi_colors = true,
+    -- colors = {
+    --     foreground = "#edeff0",
+    --     background = "#0c0e0f",
+    --     cursor_bg = "#edeff0",
+    --     cursor_fg = "#edeff0",
+    --     cursor_border = "#232526",
+    --     selection_fg = "#0c0e0f",
+    --     selection_bg = "#edeff0",
+    --     scrollbar_thumb = "#edeff0",
+    --     split = "#090909",
+    --     ansi = {"#232526", "#df5b61", "#78b892", "#de8f78", "#6791c9", "#bc83e3", "#67afc1", "#e4e6e7"},
+    --     brights = {"#2c2e2f", "#e8646a", "#81c19b", "#e79881", "#709ad2", "#c58cec", "#70b8ca", "#f2f4f5"},
+    --     indexed = {
+    --         [136] = "#edeff0"
+    --     },
+    --     tab_bar = {
+    --         active_tab = {
+    --             bg_color = "#0c0e0f",
+    --             fg_color = "#edeff0",
+    --             italic = true
+    --         },
+    --         inactive_tab = {
+    --             bg_color = "#090909",
+    --             fg_color = "#0c0e0f"
+    --         },
+    --         inactive_tab_hover = {
+    --             bg_color = "#151515",
+    --             fg_color = "#090909"
+    --         },
+    --         new_tab = {
+    --             bg_color = "#151515",
+    --             fg_color = "#090909"
+    --         },
+    --         new_tab_hover = {
+    --             bg_color = "#6791c9",
+    --             fg_color = "#090909"
+    --         }
+    --     }
+    -- },
+
+    color_scheme = 'Tokyo Night Storm (Gogh)',
+
     colors = {
-        foreground = "#edeff0",
-        background = "#0c0e0f",
-        cursor_bg = "#edeff0",
-        cursor_fg = "#edeff0",
-        cursor_border = "#232526",
-        selection_fg = "#0c0e0f",
-        selection_bg = "#edeff0",
-        scrollbar_thumb = "#edeff0",
-        split = "#090909",
-        ansi = {"#232526", "#df5b61", "#78b892", "#de8f78", "#6791c9", "#bc83e3", "#67afc1", "#e4e6e7"},
-        brights = {"#2c2e2f", "#e8646a", "#81c19b", "#e79881", "#709ad2", "#c58cec", "#70b8ca", "#f2f4f5"},
-        indexed = {
-            [136] = "#edeff0"
-        },
         tab_bar = {
             active_tab = {
-                bg_color = "#0c0e0f",
+                bg_color = "#a9b1d6",
                 fg_color = "#edeff0",
                 italic = true
             },
             inactive_tab = {
-                bg_color = "#090909",
+                bg_color = "#24283b",
                 fg_color = "#0c0e0f"
             },
-            inactive_tab_hover = {
-                bg_color = "#151515",
-                fg_color = "#090909"
-            },
+            -- inactive_tab_hover = {
+            --     bg_color = "#151515",
+            --     fg_color = "#090909"
+            -- },
             new_tab = {
                 bg_color = "#151515",
                 fg_color = "#090909"
@@ -198,8 +226,8 @@ return {
             new_tab_hover = {
                 bg_color = "#6791c9",
                 fg_color = "#090909"
-            }
-        }
+            },
+        },
     },
 
     -- Padding
@@ -214,7 +242,7 @@ return {
     enable_tab_bar = true,
     hide_tab_bar_if_only_one_tab = true,
     show_tab_index_in_tab_bar = false,
-    tab_bar_at_bottom = true,
+    tab_bar_at_bottom = false,
 
     -- General
     automatically_reload_config = true,
@@ -222,10 +250,10 @@ return {
         saturation = 1.0,
         brightness = 1.0
     },
-    window_background_opacity = 1.0,
+    window_background_opacity = 0.95,
     window_close_confirmation = "NeverPrompt",
     window_frame = {
-        active_titlebar_bg = "#090909",
+        active_titlebar_bg = "#24283b",
         font = font_with_fallback(font_name, {
             bold = true
         })
